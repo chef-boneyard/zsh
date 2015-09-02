5976567c0346376c2c835fb628817d99cb98a820
Tim Smith <tsmith84@gmail.com>
2015-09-02 11:15:42 -0700

    Rubocop fixes

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,13 @@
-name              "zsh"
-maintainer        "Chef Software, Inc."
-maintainer_email  "cookbooks@chef.io"
-license           "Apache 2.0"
-description       "Installs zsh"
-version           "1.0.1"
+name 'zsh'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Installs zsh'
+version '1.0.1'
 
 recipe 'zsh::default', 'Installs zsh.'
 
-%w{ubuntu debian centos redhat amazon scientific fedora}.each do |os|
+%w(ubuntu debian centos redhat amazon scientific fedora).each do |os|
   supports os
 end
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: zsh
 # Recipe:: default
 #
-# Copyright 2008-2009, Chef Software, Inc.
+# Copyright 2008-2015, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,17 +17,17 @@
 # limitations under the License.
 #
 
-package "zsh" do
+package 'zsh' do
   action :install
 end
 
 case node['platform_family']
-when "debian"
-  package "zsh-doc" do
+when 'debian'
+  package 'zsh-doc' do
     action :install
   end
-when "rhel", "fedora"
-  package "zsh-html" do
+when 'rhel', 'fedora'
+  package 'zsh-html' do
     action :install
   end
 end
