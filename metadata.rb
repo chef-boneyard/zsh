1f88df4dd88e1bfe9b5aa9ccbe23d8e12a635009
Tim Smith <tsmith84@gmail.com>
2015-09-02 10:59:44 -0700

    Remove duplicate metadata

--- metadata.rb
+++ metadata.rb
@@ -5,12 +5,11 @@ license           "Apache 2.0"
 description       "Installs zsh"
 version           "1.0.1"
 
-recipe "zsh", "Installs zsh"
+recipe 'zsh::default', 'Installs zsh.'
 
 %w{ubuntu debian centos redhat amazon scientific fedora}.each do |os|
   supports os
 end
-recipe 'zsh::default', 'Installs zsh.'
 
 source_url 'https://github.com/opscode-cookbooks/zsh' if respond_to?(:source_url)
 issues_url 'https://github.com/opscode-cookbooks/zsh/issues' if respond_to?(:source_url)
