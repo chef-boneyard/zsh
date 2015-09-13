f8f2a95c82b07a76f2213846bc3242ad33628234
Tim Smith <tsmith84@gmail.com>
2015-09-12 21:49:03 -0700

    Add long_description

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs zsh'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.2'
 
 recipe 'zsh::default', 'Installs zsh.'
