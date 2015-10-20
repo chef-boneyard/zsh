e5e16153db295d9e3416eb1fb5c0bfdd654d7171
Lamont Granquist <lamont@scriptkiddie.org>
2015-10-20 14:33:35 -0700

    add freebsd spec
    covers the non-rhel/ubuntu/apt/yum use case

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -27,3 +27,17 @@ describe 'default recipe on CentOS 6.5' do
     expect(chef_run).to install_multipackage_internal("collected packages install").with(package_name: ['zsh', 'zsh-html'])
   end
 end
+
+describe 'default recipe on FreeBSD 10.0' do
+  let(:chef_run) do
+    ChefSpec::ServerRunner.new(platform: 'freebsd', version: '10.0').converge('zsh::default')
+  end
+
+  it 'converges successfully' do
+    expect { :chef_run }.to_not raise_error
+  end
+
+  it 'should install zsh package' do
+    expect(chef_run).to install_multipackage_internal("collected packages install").with(package_name: ['zsh'])
+  end
+end
